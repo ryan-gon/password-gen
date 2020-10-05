dca770798ec02585b2337a35d0316a38c504a14b
ryan-gon <70781659+ryan-gon@users.noreply.github.com>
2020-10-04 20:12:56 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,71 @@
+
+function getRandomUpperCase(){
+    return String.fromCharCode(Math.floor(Math.random()*26)+65);
+   }
+
+   function getRandomLowerCase(){
+    return String.fromCharCode(Math.floor(Math.random()*26)+97);
+ }
+
+ function getRandomNumber(){
+  return String.fromCharCode(Math.floor(Math.random()*10)+48);
+}
+
+function getRandomSymbol(){
+  var symbol = "!@#$%^&*(){}[]=<>/,.|~?";
+  return symbol[Math.floor(Math.random()*symbol.length)];
+}
+
+var answerEl = document.getElementById("answer");
+var lengthEl = document.getElementById("length");
+var numberEl = document.getElementById("number");
+var lowerEl = document.getElementById("lower");
+var upperEl = document.getElementById("upper");
+var symbolEl = document.getElementById("symbol");
+var copyEl = document.getElementById("copy");
+var generateEl = document.getElementById("generate");
+
+const randomFunc = {
+    upper : getRandomUpperCase,
+    lower : getRandomLowerCase,
+    number : getRandomNumber,
+    symbol : getRandomSymbol
+};
+
+function generateclick() {
+    const length = +lengthEl.value;
+    const hasUpper = upperEl.checked;
+    const hasLower = lowerEl.checked;
+    const hasNumber = numberEl.checked;
+    const hasSymbol = symbolEl.checked;
+
+answerEl.innerText = generatePassword(hasUpper, hasLower, hasNumber, hasSymbol, length);
+}
+
+
+
+function generatePassword(upper, lower, number, symbol, length){
+    let generatedPassword = "";
+console.log('Generating');
+    const typesCount = upper + lower + number + symbol;
+
+    //console.log(typesCount);
+
+    const typesArr = [{upper}, {lower}, {number}, {symbol}].filter(item => Object.values(item)[0]);
+
+    if(typesCount === 0) {
+        return '';
+    }
+
+    for(let i=0; i<length; i+=typesCount) {
+        typesArr.forEach(type => {
+            const funcName = Object.keys(type)[0];
+            generatedPassword += randomFunc[funcName]();
+        });
+    }
+
+    const finalPassword = generatedPassword.slice(0, length);
+
+
+    return finalPassword;
+}
